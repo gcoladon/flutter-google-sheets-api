233d5d1bd94ee6c0b5672ba9d01e0f375675e3da
Greg Coladonato <gcoladonato3@gatech.edu>
2024-05-04 13:55:48 -0700

    removed and commented out code to make VS Code happy

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gsheets/gsheets.dart';
 import 'package:sheets_backend/configs/secrets.dart';
 import 'package:sheets_backend/providers/google_sheets_provider.dart';
-import 'package:sheets_backend/views/app.dart';
+// import 'package:sheets_backend/views/app.dart';
 import 'package:sheets_backend/views/generic.dart';
 import 'package:logging/logging.dart';
 
@@ -14,48 +14,50 @@ void main() async {
     print('${record.level.name}: ${record.time}: ${record.message}');
   });
 
-  const useGeneric = true;
+/*   const useGeneric = true;
 
   if (useGeneric) {
-    final List<String> tables = [];
-    final Map<String, List<String>> columns = {};
-    final Map<String, List<String>> relations = {};
+ */
+  final List<String> tables = [];
+  final Map<String, List<String>> columns = {};
+  final Map<String, List<String>> relations = {};
 
-    final gsheets = GSheets(credentials);
-    final ss = await gsheets.spreadsheet(sheetId);
-    final ssTitle = ss.data.properties.title ?? "Unknown";
+  final gsheets = GSheets(credentials);
+  final ss = await gsheets.spreadsheet(sheetId);
+  final ssTitle = ss.data.properties.title ?? "Unknown";
 
-    for (final sheet in ss.sheets) {
-      final title = sheet.title;
-      columns[title] = await sheet.values.row(1);
+  for (final sheet in ss.sheets) {
+    final title = sheet.title;
+    columns[title] = await sheet.values.row(1);
 
-      if (!title.contains("_")) {
-        tables.add(title);
-      } else {
-        for (final member in title.split("_")) {
-          if (!relations.containsKey(member)) {
-            relations[member] = [];
-          }
-          relations[member]!.add(title);
+    if (!title.contains("_")) {
+      tables.add(title);
+    } else {
+      for (final member in title.split("_")) {
+        if (!relations.containsKey(member)) {
+          relations[member] = [];
         }
+        relations[member]!.add(title);
       }
     }
-    // print ("Found relations: $relations");
-    gsheets.close();
+  }
+  // print ("Found relations: $relations");
+  gsheets.close();
 
-    await provider.initializeForWorksheet(sheetId);
+  await provider.initializeForWorksheet(sheetId);
 
-    runApp(GenericApp(
-        provider: provider,
-        appName: ssTitle,
-        tables: tables,
-        columns: columns,
-        relations: relations));
-  } else {
+  runApp(GenericApp(
+      provider: provider,
+      appName: ssTitle,
+      tables: tables,
+      columns: columns,
+      relations: relations));
+/*   } else {
     await provider.initializeForWorksheet(sheetId);
 
     runApp(SheetsApp(
       provider: provider,
     ));
   }
+ */
 }
